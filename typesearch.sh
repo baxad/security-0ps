928406710b10b49dee3dce74b065bbcc7dfaed09
baxad <mahammadreza.esmaeili94@gmail.com>
2020-05-08 07:35:50 -0700

    comm-x01

---
+++ typesearch.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -
+#
+# Search the file system for a given file type. It prints out the
+# path or copies files to desired directory DESTDIR
+#
+# Usage:
+# typesearch.sh [-c dir] [-i] [-R|r] <pattern> <path>
+# -c Copy files found to dir
+# -i Ignore case
+# -R|r Recursively search subdirectories
+# <pattern> File type pattern to search for
+# <path> Path to start search
+#
+DEEPORNOT="-maxdepth 1" # just the current dir; default
+# PARSE option arguments:
+while getopts 'c:irR' opt; do
+case "${opt}" in
+c) # copy found files to specified directory
+COPY=YES
+DESTDIR="$OPTARG"
+;;
+i) # ignore u/l case differences in search
+CASEMATCH='-i'
+;;
+[Rr]) # recursive
+unset DEEPORNOT;;
+*) # unknown/unsupported option
+# error mesg will come from getopts, so just exit
+exit 2 ;;
+esac
+done
+shift $((OPTIND - 1))
+PATTERN=${1:-PDF document}
+STARTDIR=${2:-.} # by default start here
+find $STARTDIR $DEEPORNOT -type f | while read FN
+do
+file $FN | egrep -q $CASEMATCH "$PATTERN"
+if (( $? == 0 )) # found one
+then
+echo $FN
+if [[ $COPY ]]
+then
+cp -p $FN $DESTDIR
+fi
+fi
+done
+
