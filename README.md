9fe0a941ffda23871b22da8a82e561c920b85095
baxad <mahammadreza.esmaeili94@gmail.com>
2020-05-08 06:19:14 -0700

    comm-x0

---
+++ README.md
@@ -0,0 +1,2 @@
+welcome to security-0ps this repo contains my personal scripts created for cyber security operations as parts of my own personal trainings and poking around diffrent security acts 
+each script is defined using comments and examples in files some need other files as input wich are included inside the repository feel free to use and contribute to my projects
---
+++ bannergrabber.sh
@@ -0,0 +1,59 @@
+#!/bin/bash -
+#
+# bannergrabber.sh
+#
+# Description:
+#Simple script to Automatically pull the banners from HTTP, SMTP,
+# and FTP servers
+#
+# Usage: ./bannergrabber.sh hostname [scratchfile]
+# scratchfile is used during processing but removed;
+# default is: "scratch.file" or tempfile-generated name
+#
+#
+function isportopen ()
+{
+(( $# < 2 )) && return 1
+local host port
+host=$1
+port=$2
+echo >/dev/null 2>&1 < /dev/tcp/${host}/${port}
+return $?
+}
+function cleanup ()
+{
+rm -f "$SCRATCH"
+}
+ATHOST="$1"
+SCRATCH="$2"
+if [[ -z $2 ]]
+then
+if [[ -n $(type -p tempfile) ]]
+then
+SCRATCH=$(tempfile)
+else
+SCRATCH='scratch.file'
+fi
+fi
+trap cleanup EXIT
+touch "$SCRATCH"
+if isportopen $ATHOST 21 # FTP
+then
+# i.e., ftp -n $ATHOST
+exec 3<>/dev/tcp/${ATHOST}/21
+echo -e 'quit\r\n' >&3
+cat <&3 >> "$SCRATCH"
+fi
+if isportopen $ATHOST 25 # SMTP
+then
+# i.e., telnet $ATHOST 25
+exec 3<>/dev/tcp/${ATHOST}/25
+echo -e 'quit\r\n' >&3
+cat <&3 >> "$SCRATCH"
+fi
+if isportopen $ATHOST 80 # HTTP
+then
+curl -LIs "https://${ATHOST}" >> "$SCRATCH"
+fi
+cat "$SCRATCH"
+
