6967dfbd773c5a18cbc5d0afca39e88382e0d67a
baxad <mahammadreza.esmaeili94@gmail.com>
2020-05-09 06:00:17 -0700

    commx02

---
+++ bash-src/hashsearch.sh
@@ -0,0 +1,30 @@
+# matches a given SHA-1 hash
+#
+# Usage:
+# hashsearch.sh <hash> <directory>
+# hash - SHA-1 hash value to file to find
+# directory - Top directory to start search
+#
+HASH=$1
+DIR=${2:-.} # default is here, cwd
+# convert pathname into an absolute path
+function mkabspath ()
+{
+if [[ $1 == /* ]]
+then
+ABS=$1
+else
+ABS="$PWD/$1"
+fi
+}
+find $DIR -type f |
+while read fn
+do
+THISONE=$(sha1sum "$fn")
+THISONE=${THISONE%% *}
+if [[ $THISONE == $HASH ]]
+then
+mkabspath "$fn"
+echo $ABS
+fi
+done
